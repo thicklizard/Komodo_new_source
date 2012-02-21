6264a5971b8dfeadf146adee8ea6605805c15c2c
Jordan Crouse <jcrouse@codeaurora.org>
2012-02-21 08:54:52 -0700

    msm: kgsl: Program both IOMMUs at our disposal on 8064
    APQ8064 has two AXI ports attached to the GPU with an IOMMU on both.
    The VBIF can be programmed to use a round-robin arbitration to access
    the ports equally, so we need to program both IOMMUs with the same
    pagetable to make sure that the GPU can get where it needs to go.
    All this involves a board file infrastructure change that affects 8960
    too, but no functional changes are happening for that platform.
    
    Change-Id: Ic0dedbad5198ed9c453711e58a3f1220ef5d5b4e
    Signed-off-by: Jordan Crouse <jcrouse@codeaurora.org>

--- arch/arm/mach-msm/devices-8960.c
+++ arch/arm/mach-msm/devices-8960.c
@@ -2731,6 +2731,20 @@ static struct resource kgsl_3d0_resources[] = {
 	},
 };
 
+static const char *kgsl_3d0_iommu_ctx_names[] = {
+	"gfx3d_user",
+	/* priv_ctx goes here */
+};
+
+static struct kgsl_device_iommu_data kgsl_3d0_iommu_data[] = {
+	{
+		.iommu_ctx_names = kgsl_3d0_iommu_ctx_names,
+		.iommu_ctx_count = ARRAY_SIZE(kgsl_3d0_iommu_ctx_names),
+		.physstart = 0x07C00000,
+		.physend = 0x07C00000 + SZ_1M - 1,
+	},
+};
+
 static struct kgsl_device_platform_data kgsl_3d0_pdata = {
 	.pwrlevel = {
 		{
@@ -2768,6 +2782,8 @@ static struct kgsl_device_platform_data kgsl_3d0_pdata = {
 #ifdef CONFIG_MSM_BUS_SCALING
 	.bus_scale_table = &grp3d_bus_scale_pdata,
 #endif
+	.iommu_data = kgsl_3d0_iommu_data,
+	.iommu_count = ARRAY_SIZE(kgsl_3d0_iommu_data),
 };
 
 struct platform_device msm_kgsl_3d0 = {
@@ -2792,6 +2808,19 @@ static struct resource kgsl_2d0_resources[] = {
 		.start = GFX2D0_IRQ,
 		.end = GFX2D0_IRQ,
 		.flags = IORESOURCE_IRQ,
+	},
+};
+
+static const char *kgsl_2d0_iommu_ctx_names[] = {
+	"gfx2d0_2d0",
+};
+
+static struct kgsl_device_iommu_data kgsl_2d0_iommu_data[] = {
+	{
+		.iommu_ctx_names = kgsl_2d0_iommu_ctx_names,
+		.iommu_ctx_count = ARRAY_SIZE(kgsl_2d0_iommu_ctx_names),
+		.physstart = 0x07D00000,
+		.physend = 0x07D00000 + SZ_1M - 1,
 	},
 };
 
@@ -2828,6 +2857,8 @@ static struct kgsl_device_platform_data kgsl_2d0_pdata = {
 #ifdef CONFIG_MSM_BUS_SCALING
 	.bus_scale_table = &grp2d0_bus_scale_pdata,
 #endif
+	.iommu_data = kgsl_2d0_iommu_data,
+	.iommu_count = ARRAY_SIZE(kgsl_2d0_iommu_data),
 };
 
 struct platform_device msm_kgsl_2d0 = {
@@ -2837,6 +2868,19 @@ struct platform_device msm_kgsl_2d0 = {
 	.resource = kgsl_2d0_resources,
 	.dev = {
 		.platform_data = &kgsl_2d0_pdata,
+	},
+};
+
+static const char *kgsl_2d1_iommu_ctx_names[] = {
+	"gfx2d0_2d1",
+};
+
+static struct kgsl_device_iommu_data kgsl_2d1_iommu_data[] = {
+	{
+		.iommu_ctx_names = kgsl_2d1_iommu_ctx_names,
+		.iommu_ctx_count = ARRAY_SIZE(kgsl_2d1_iommu_ctx_names),
+		.physstart = 0x07E00000,
+		.physend = 0x07E00000 + SZ_1M - 1,
 	},
 };
 
@@ -2888,6 +2932,8 @@ static struct kgsl_device_platform_data kgsl_2d1_pdata = {
 #ifdef CONFIG_MSM_BUS_SCALING
 	.bus_scale_table = &grp2d1_bus_scale_pdata,
 #endif
+	.iommu_data = kgsl_2d1_iommu_data,
+	.iommu_count = ARRAY_SIZE(kgsl_2d1_iommu_data),
 };
 
 struct platform_device msm_kgsl_2d1 = {
