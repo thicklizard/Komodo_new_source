990956b7a6741beb7c8e3f669c41d9b0d958b400
Carter Cooper <ccooper@codeaurora.org>
2012-06-05 11:18:34 -0600

    Remove last_expired_ctxt_id

--- drivers/gpu/msm/kgsl.c
+++ drivers/gpu/msm/kgsl.c
@@ -448,6 +448,7 @@ void kgsl_timestamp_expired(struct work_struct *work)
 		kfree(event);
 	}
 
+<<<<<<< HEAD
 	/* Send the next pending event for each context to the device */
 	if (device->ftbl->next_event) {
 		unsigned int id = KGSL_MEMSTORE_GLOBAL;
@@ -464,6 +465,8 @@ void kgsl_timestamp_expired(struct work_struct *work)
 		}
 	}
 
+=======
+>>>>>>> c55448d... msm: kgsl: Remove last_expired_ctxt_id
 	mutex_unlock(&device->mutex);
 }
 EXPORT_SYMBOL(kgsl_timestamp_expired);
