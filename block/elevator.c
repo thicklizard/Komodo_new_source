0f281ab89f8071424f2b62d27cb49100eecf2b6a
thicklizard <thicklizard@gmail.com>
2012-12-02 06:23:45 -0600

    tweak elevator

--- block/elevator.c
+++ block/elevator.c
@@ -353,7 +353,7 @@ static struct request *elv_rqhash_find(struct request_queue *q, sector_t offset)
  * RB-tree support functions for inserting/lookup/removal of requests
  * in a sorted RB tree.
  */
-void elv_rb_add(struct rb_root *root, struct request *rq
+void elv_rb_add(struct rb_root *root, struct request *rq)
 {
 	struct rb_node **p = &root->rb_node;
 	struct rb_node *parent = NULL;
