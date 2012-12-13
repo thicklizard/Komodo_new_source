c19d4cca9b6670d4d2f91242beff0f82c1d65150
Anji Jonnala <anjir@codeaurora.org>
2012-12-13 14:03:54 +0530

    msm: cpufreq: Implement suspend/resume for cpufreq driver

--- drivers/cpufreq/cpufreq_ondemand.c
+++ drivers/cpufreq/cpufreq_ondemand.c
@@ -922,9 +922,20 @@ static void dbs_refresh_callback(struct work_struct *unused)
 		return;
 	}
 
+
 	if (policy->cur < DBS_INPUT_EVENT_MIN_FREQ) {
 		__cpufreq_driver_target(policy, DBS_INPUT_EVENT_MIN_FREQ,
 					CPUFREQ_RELATION_L);
+
+	if (policy->cur < policy->max) {
+		/*
+		 * Arch specific cpufreq driver may fail.
+		 * Don't update governor frequency upon failure.
+		 */
+		if (__cpufreq_driver_target(policy, policy->max,
+					CPUFREQ_RELATION_L) >= 0)
+			policy->cur = policy->max;
+
 		this_dbs_info->prev_cpu_idle = get_cpu_idle_time(cpu,
 				&this_dbs_info->prev_cpu_wall);
 	}
