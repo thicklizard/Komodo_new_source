b31c75dac292fbc84dc53f86ca0747ea1f9d1071
thicklizard <thicklizard@gmail.com>
2013-02-11 20:32:42 -0600

    merge from 4.2

--- arch/arm/mach-msm/devices-8960.c
+++ arch/arm/mach-msm/devices-8960.c
@@ -2670,12 +2670,12 @@ static struct kgsl_device_platform_data kgsl_3d0_pdata = {
 		{
 			 .gpu_freq = 512000000,
                          .bus_freq = 4,
-                         .io_fraction = 33,
+                         .io_fraction = 0,
                  },
                  {
 			.gpu_freq = 400000000,
 			.bus_freq = 3,
-			.io_fraction = 50,
+			.io_fraction = 33,
 		 },
 		 {
 			.gpu_freq = 300000000,
@@ -2693,9 +2693,9 @@ static struct kgsl_device_platform_data kgsl_3d0_pdata = {
 <<<<<<< HEAD
 		 },
 		 },
-	.init_level = 1,
-	.max_level = 2,
+	.init_level = 2,
 	.num_levels = 5,
+<<<<<<< HEAD
 =======
 		},
 	},
@@ -2703,6 +2703,9 @@ static struct kgsl_device_platform_data kgsl_3d0_pdata = {
 	.num_levels = 5,
 	.max_level= 6,
 >>>>>>> 085b8a3... fix error i made on last commit
+=======
+	.max_level= 6,
+>>>>>>> e4b739d... rework some things for aroma
 	.set_grp_async = NULL,
 	.idle_timeout = HZ/12,
 	.nap_allowed = false,
@@ -2763,12 +2766,10 @@ static struct kgsl_device_platform_data kgsl_2d0_pdata = {
  			.bus_freq = 0,
 		},
 	},
-<<<<<<< HEAD
+
 	.init_level = 1,
 	.max_level = 2,
-=======
 	.init_level = 0,
->>>>>>> 085b8a3... fix error i made on last commit
 	.num_levels = 5,
 	.set_grp_async = NULL,
 	.idle_timeout = HZ/12,
@@ -2831,12 +2832,17 @@ static struct kgsl_device_platform_data kgsl_2d1_pdata = {
 		},
 	},
 <<<<<<< HEAD
+<<<<<<< HEAD
 	.init_level = 1,
 	.max_level = 2,
 =======
 	.init_level = 0,
 	.max_level=1,
 >>>>>>> 085b8a3... fix error i made on last commit
+=======
+	.init_level = 0,
+	.max_level=1,
+>>>>>>> e4b739d... rework some things for aroma
 	.num_levels = 5,
 	.set_grp_async = NULL,
 	.idle_timeout = HZ/12,
