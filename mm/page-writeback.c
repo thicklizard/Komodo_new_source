c2bac37b2c9938d6ed15dbe14eca8a09b5a2fbc7
thicklizard <thicklizard@gmail.com>
2013-03-03 08:40:42 -0600

    revert old commit

--- mm/page-writeback.c
+++ mm/page-writeback.c
@@ -35,9 +35,6 @@
 #include <linux/buffer_head.h>
 #include <linux/pagevec.h>
 #include <trace/events/writeback.h>
-#include <linux/earlysuspend.h>
-
-
 
 /*
  * After a CPU has dirtied this many pages, balance_dirty_pages_ratelimited
@@ -93,7 +90,7 @@ unsigned long vm_dirty_bytes;
 /*
  * The interval between `kupdate'-style writebacks
  */
-unsigned int dirty_writeback_interval = 0; /* centiseconds */
+unsigned int dirty_writeback_interval = 5 * 100; /* centiseconds */
 
 /*
  * The longest time for which data is allowed to remain dirty
@@ -776,21 +773,6 @@ static struct notifier_block __cpuinitdata ratelimit_nb = {
 	.next		= NULL,
 };
 
-static void dirty_early_suspend(struct early_suspend *handler)
-{
-	dirty_writeback_interval = 5 * 100;
-}
-
-static void dirty_late_resume(struct early_suspend *handler)
-{
-	dirty_writeback_interval = 0;
-}
-
-static struct early_suspend dirty_suspend = {
-	.suspend = dirty_early_suspend,
-	.resume = dirty_late_resume,
-};
-
 /*
  * Called early on to tune the page writeback dirty limits.
  *
@@ -812,8 +794,6 @@ static struct early_suspend dirty_suspend = {
 void __init page_writeback_init(void)
 {
 	int shift;
-
-	register_early_suspend(&dirty_suspend);
 
 	writeback_set_ratelimit();
 	register_cpu_notifier(&ratelimit_nb);
